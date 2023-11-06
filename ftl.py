04d67105a3be4d31006b691b664895f7ad035877
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-11-06 17:47:47 +0800

    Added file transliteration support

--- ftl.py
+++ ftl.py
@@ -1,2 +1,23 @@
-import translinerate
+from transliterate import translit
+import argparse
+import os 
 
+parser = argparse.ArgumentParser(description='Script for file names transliteration')
+
+parser.add_argument('filenames', type=str, nargs='*', metavar='Filename', 
+                    help='Files to transliterate')
+
+parser.add_argument('--english', type=bool, metavar='LANG', default=True,
+                    help='Trasliterate to English, default is True')
+
+parser.add_argument('--lang', type=str, metavar='LANG', required=True,
+                    help='Target language')
+
+args = parser.parse_args()
+
+for filename in args.filenames:
+    if os.path.exists(filename):
+        new_filename = translit(filename, args.lang, reversed=args.english)
+        os.rename(filename, new_filename)
+    else:
+        print("File {filename} not found.")
